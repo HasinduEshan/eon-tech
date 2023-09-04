c195f50092c15b1735a128c0888996716175a5d4
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-09-04 14:41:10 +0530

    service content basic page added

--- src/page_body/PageBody.js
+++ src/page_body/PageBody.js
@@ -4,21 +4,27 @@ import React from 'react'
 import HomePage from '../home_page/HomePage';
 import AboutUs from '../about_us/AboutUs';
 import ResponsiveNavBar from '../nav_bar/ResponsiveNavBar';
+import Services from '../services/Services';
 
 export default function PageBody() {
   return (
     <div>
       <ResponsiveNavBar home='home' about_us='about-us' />
       <section id='home' className='cover'>
-        <HomePage/>
+        <HomePage />
       </section>
       <section id='about-us' className='container-fluid'>
         <div className='row'>
-          <div className='second-cover col-sm-12 col-md-6'>
-            <AboutUs/>
+          <div className='about-us-cover col-sm-12 col-md-6'>
+            <AboutUs />
           </div>
         </div>
       </section>
+      <section id='services' className='services-cover'>
+        Our Services
+        <Services/>
+      </section>
+
     </div>
   )
 }
---
+++ src/services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './custom.css'
+
+export default function Services() {
+  return (
+      <dive className='second-div'>
+        <div className='second-cover'>
+          
+        </div>
+      </dive>
+    
+  )
+}
