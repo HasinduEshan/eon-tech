8a219cc3c25d48a1dfa58171d1c60480977c9092
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-09-04 12:42:08 +0530

    home and about us completed

--- src/main_page/MainPage.js
+++ src/main_page/MainPage.js
@@ -1,9 +1,7 @@
 import React from 'react'
 // import ResponsiveNavBar from '../nav_bar/ResponsiveNavBar';
 import './custom.css';
-// import HomePage from '../home_page/HomePage';
 import PageBody from '../page_body/PageBody';
-// import AboutUs from './../about_us/AboutUs';
 import './../page_body/custom.css';
 
 export default function MainPage() {
